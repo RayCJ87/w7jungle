f9f4c8773abce5832c73d1445f9435505b8a7511
RayCJ87 <shihcr.ke@gmail.com>
2019-01-21 22:27:29 +0000

    product testing added

---
+++ spec/product_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    # validation tests/examples here
+    before do
+      @category = Category.find_or_create_by! name: 'Furniture'
+      @product = @category.products.create!({
+        name: "Macbook wild",
+        description: 'what a wonderful product!',
+        quantity: 15,
+        price: 3999.99
+      })
+    end
+
+    it "should be invalid if the name field if empty" do
+      @product.name = nil
+      expect(@product).to_not be_valid
+    end
+
+    it 'should be invalid if the category field is empty' do
+      @product.category = nil
+      expect(@product).to be_valid
+      expect(@product.errors.full_messages[0]).to eq('Category can\'t be blank')
+    end
+
+  end
+end
